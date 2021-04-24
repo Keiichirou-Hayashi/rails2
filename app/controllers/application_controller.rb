296da480c0d906605e979c80f0e2ec4722644b32
【林敬一郎】 <k.h.526@i.softbank.jp>
2021-04-24 10:15:14 +0900

    途中経過

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  autocomplete :user, :username, full: true
+
+  def after_sign_in_path_for(resource)
+    user_path(resource.id)
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  root 'rooms#index'
+
+  devise_for :users
+
+  resources :users, only: [:show]
+  
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'users/index'
+
+  resources :users do
+    get :autocomplete_user_username, on: :collection
+  end
+  resources :users
+  resources :rooms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
